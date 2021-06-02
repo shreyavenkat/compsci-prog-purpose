9f1bd8de9394a8879386727b5681b10e3ba28327
shreyavenkat <70924053+shreyavenkat@users.noreply.github.com>
2021-06-02 12:13:41 -0700

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+/* This code prints the words Hello World to the screen */
+
+public class HelloWorld 
+{
+	public static void main (String args [])
+	{
+		System.out.println("Hello, World");
+	}
+
+}
